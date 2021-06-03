aaab5a80f864595d0d815c40b4444dc92508a94f
2394425147 <2394425147@qq.com>
2021-06-04 00:04:07 +0800

    Split up the panels & Added preview
    Preparations for download button also added

---
+++ Download.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ksm_download.JsonFormats;
+
+namespace ksm_download
+{
+    static class Download
+    {
+        public static async Task<bool> downloadFile(string url, string path, Dictionary<string, string> keypair = null)
+        {
+            WebClient wc = new WebClient();
+            if (keypair != null)
+            {
+                foreach (var item in keypair)
+                {
+                    wc.Headers.Add(item.Key, item.Value);
+                }
+            }
+            Console.WriteLine("downloading");
+            try
+            {
+                byte[] data = await wc.DownloadDataTaskAsync(url);
+                Console.WriteLine("saving");
+                File.WriteAllBytes(path, data);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed." + e.Message);
+                return false;
+            }
+        }
+
+        public static async Task<T> downloadJson<T>(string url, Dictionary<string, string> keypair = null)
+        {
+            return JsonConvert.DeserializeObject<T>(await downloadText(url, keypair));
+        }
+
+        public static async Task<string> downloadText(string url,Dictionary<string,string> keypair = null)
+        {
+            WebClient wc = new WebClient();
+            if (keypair != null)
+            {
+                foreach(var item in keypair)
+                {
+                    wc.Headers.Add(item.Key, item.Value);
+                }
+            }
+
+            Console.WriteLine("downloading " + url);
+            try
+            {
+                byte[] data = await wc.DownloadDataTaskAsync(url);
+                Console.WriteLine("saving");
+                string text = Encoding.UTF8.GetString(data);
+                return text;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed." + e.Message);
+                return default;
+            }
+        }
+
+        public static async Task<string> login(string url, string username,string password)
+        {
+            WebClient wc = new WebClient();
+            try
+            {
+                string jsondata = JsonConvert.SerializeObject(new LoginJson(username, password));
+                byte[] buffer = Encoding.UTF8.GetBytes(jsondata);
+                byte[] data = await wc.UploadDataTaskAsync(url, "POST", buffer);
+                string text = Encoding.UTF8.GetString(data);
+                SongListJsonRoot js = JsonConvert.DeserializeObject<SongListJsonRoot>(text);
+                if (js.Code != 200)
+                {
+                    return js.Code.ToString();
+                }
+                return wc.ResponseHeaders.Get("Set-Cookie");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed." + e.Message);
+                return default;
+            }
+            
+        }
+    }
+}
---
+++ SongData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksm_download
+{
+    /// <summary>
+    /// 和listview绑定的类
+    /// </summary>
+    public class song
+    {
+        public Guid id { get; set; }
+        public string downState { get; set; }
+        public string name { get; set; }
+        public string[] levels { get; set; }
+        public string artist { get; set; }
+        public string charter { get; set; }
+        public song(string _id, string _name, string[] _lv, string _art, string _charter)
+        {
+            id = new Guid(_id);
+            name = _name;
+            artist = _art;
+            levels = _lv;
+            charter = _charter;
+        }
+    }
+
+    public class SongData : INotifyPropertyChanged
+    {
+        List<song> _songslist = new List<song>();
+        public List<song> songslist
+        {
+            get
+            {
+                return _songslist;
+            }
+            set
+            {
+                OnPropertyChanged("已下载");
+                _songslist = songslist;
+            }
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
